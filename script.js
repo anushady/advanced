e97400980f1601059a71eb1656dd423fb7151dc8
Anwar M. Abrar <88562556+anushady@users.noreply.github.com>
2022-06-12 06:11:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ var loader = new THREE.GLTFLoader();
 var obj;
 loader.load(
   // resource URL
-  "models/a.glb",
+  "models/whatsup3.glb",
   // called when the resource is loaded
   function (gltf) {
     obj = gltf.scene;
     scene.add(obj);
-    obj.scale.set(0.026, 0.026, 0.026);
-    obj.position.set(0, 0, 0);
+    obj.scale.set(0.9, 0.9, 0.9);
+    obj.position.set(0, 0.15, 0);
   }
 );
 // const video = document.getElementById("video");
@@ -31,12 +31,78 @@ loader.load(
 //   transparent: true,
 //   opacity: 0,
 // });
-
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 // const plane = new THREE.Mesh(geometry, material);
 // scene.add(plane);
 // plane.position.set(0, 0, -9);
 // plane.rotation.set(0, Math.PI / 2, 0);
+let textureVid = document.createElement("video");
+textureVid.src = `images/showreel_preview.mp4`; // transform gif to mp4
+textureVid.loop = true;
+window.addEventListener("mousemove", () => {
+  textureVid.play();
+});
 
+// Load video texture
+let videoTexture = new THREE.VideoTexture(textureVid);
+videoTexture.format = THREE.RGBFormat;
+videoTexture.minFilter = THREE.NearestFilter;
+videoTexture.maxFilter = THREE.NearestFilter;
+videoTexture.generateMipmaps = false;
+//const video = document.getElementById("video");
+//const texture = new THREE.VideoTexture(video);
+//texture.minFilter = THREE.LinearFilter;
+//texture.magFilter = THREE.LinearFilter;
+const imagetex = new THREE.TextureLoader().load("images/restricted.gif");
+let geometry = new THREE.PlaneGeometry(5, 3);
+const material = new THREE.MeshBasicMaterial({
+  map: videoTexture,
+  //color: 0xffff00,
+  side: THREE.DoubleSide,
+  transparent: true,
+  opacity: 0,
+});
+
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+plane.position.set(0, -0.5, -9);
+plane.rotation.set(0, Math.PI / 2, 0);
+plane.scale.set(1, 1, 1);
+
+var widtha = $(window).width();
+if (widtha < 1000) {
+  plane.scale.set(0.8, 0.8, 1);
+}
+var widtha = $(window).width();
+if (widtha < 800) {
+  plane.scale.set(0.55, 0.55, 1);
+}
+var widtha = $(window).width();
+if (widtha < 600) {
+  plane.scale.set(0.4, 0.4, 1);
+}
+
+$(window).resize(function () {
+  var widtha = $(window).width();
+  if (widtha < 1000) {
+    plane.scale.set(0.8, 0.8, 1);
+  }
+});
+$(window).resize(function () {
+  var widtha = $(window).width();
+  if (widtha < 800) {
+    plane.scale.set(0.55, 0.55, 1);
+  }
+});
+$(window).resize(function () {
+  var widtha = $(window).width();
+  if (widtha < 600) {
+    plane.scale.set(0.4, 0.4, 1);
+  }
+});
 // Lights
 const light = new THREE.AmbientLight(0xffffff, 0.6); // soft white light
 scene.add(light);
@@ -71,10 +137,6 @@ scene.add(rectLight2);
 /**
  * Sizes
  */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
 
 window.addEventListener("resize", () => {
   // Update sizes
@@ -149,13 +211,19 @@ function onDocumentMouseMove(event) {
 
 const updateOnScroll = (event) => {
   let tl = gsap.timeline();
-  tl.to(obj.rotation, {
-    y: 0,
-    duration: 1,
-  }).to(
+
+  tl.to(
+    obj.rotation,
+    {
+      y: 0,
+      duration: 1,
+    },
+    0
+  ).to(
     obj.position,
     {
       x: 0,
+      y: 0,
       z: window.scrollY * 0.0033,
     },
     0
@@ -164,37 +232,53 @@ const updateOnScroll = (event) => {
 
 window.addEventListener("scroll", updateOnScroll);
 
-// var action6 = gsap.to(".webgl", {
-//   ease: "none",
-//   duration: 0.25,
-// });
+document.addEventListener("mousemove", onDocumentMouseMove2);
 
-// ScrollTrigger.create({
-//   trigger: canvas2,
-//   start: "top top",
-//   endTrigger: "#section3",
-//   end: "top top",
-//   pin: true,
-//   pinSpacing: false,
-//   animation: action6,
-//   toggleActions: "play reverse play reverse",
-// });
+let mouseX4 = 0;
+let mouseY4 = 0;
 
-// var tl0 = gsap.timeline();
-// var action5 = tl0.to(material, { opacity: 1, duration: 1 }, 0);
-// tl0.to(plane.rotation, { y: Math.PI, duration: 1 }, 0);
-// tl0.to(plane.position, { z: -2, duration: 1 }, 0);
-// tl0.to(plane.position, { x: -0.2 }, 0);
-// ScrollTrigger.create({
-//   trigger: "#section2",
-//   start: "top top",
-//   endTrigger: "#section3",
-//   end: "top top",
-//   animation: action5,
-//   scrub: 2,
-//   toggleActions: "play reverse play reverse",
-// });
+let targetX4 = 0;
+let targetY4 = 0;
 
+const windowX4 = window.innerWidth / 2;
+const windowY4 = window.innerHeight / 2;
+
+function onDocumentMouseMove2(event) {
+  mouseX4 = event.clientX - windowX4;
+  mouseY4 = event.clientY - windowY4;
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+var tl0 = gsap.timeline();
+var action5 = tl0.to(material, { opacity: 1, duration: 1 }, 0);
+
+tl0.to(plane.rotation, { y: Math.PI, duration: 1 }, 0);
+tl0.to(plane.position, { z: -2, duration: 1 }, 0);
+tl0.to(plane.position, { x: 0, duration: 1 }, 0);
+
+ScrollTrigger.create({
+  trigger: "#section2",
+  start: "top top",
+  endTrigger: "#section3",
+  end: "top top",
+  animation: action5,
+  scrub: 2,
+  toggleActions: "play reverse play reverse",
+});
+
+var tl0y = gsap.timeline();
+var actiony = tl0y.to(plane.position, { y: 3.57 }, 0);
+
+ScrollTrigger.create({
+  trigger: "#section3",
+  start: "top top",
+  endTrigger: "#section4",
+  end: "top top",
+  animation: actiony,
+  scrub: true,
+  toggleActions: "play reverse play reverse",
+});
 const clock = new THREE.Clock();
 
 const tick = () => {
@@ -205,8 +289,10 @@ const tick = () => {
   targetX = mouseX * 0.001;
   targetY = mouseY * 0.001;
 
+  targetX4 = mouseX4 * 0.0003;
+  targetY4 = mouseY4 * 0.0003;
   // Update objects
-  if (obj) obj.rotation.y += 0.02 * (targetX - obj.rotation.y);
+  if (obj) obj.rotation.y += 0.008 * (targetX - obj.rotation.y);
   //plane.rotation.y -= 0.004 * (targetX - plane.rotation.z);
   //plane.rotation.x -= 0.00006 * (targetY - plane.rotation.y);
   //if (obj) obj.position.z += 0.0008 * (targetY - obj.position.z);
@@ -220,12 +306,14 @@ const tick = () => {
 tick();
 
 // move Arrow png with cursor on image hover
-gsap.utils.toArray(".container").forEach((el) => {
+gsap.utils.toArray(".recentworkcontainer").forEach((el) => {
   const image = el.querySelector("img.swipeimage"),
+    //const image2 = el.querySelector("img.imagediv"),
     setX = gsap.quickSetter(image, "x", "px"),
     setY = gsap.quickSetter(image, "y", "px"),
     align = (e) => {
       const top = el.getBoundingClientRect().top;
+      //const left = el.getBoundingClientRect().left;
       setX(e.clientX);
       setY(e.clientY - top);
     },
@@ -305,8 +393,8 @@ var hoverMouse = function ($el) {
 
     var onHover = function (x, y) {
       gsap.to($self, 0.4, {
-        x: x * 0.8,
-        y: y * 0.8,
+        x: x * 0.3,
+        y: y * 0.3,
         //scale: .9,
         rotation: x * 0.05,
         ease: Power2.easeOut,
@@ -329,8 +417,9 @@ var hoverMouse = function ($el) {
 hoverMouse($(".link"));
 
 //image Zoom
-//const imagediv = document.querySelector(".img");
 
-// document.querySelector(".imagediv").addEventListener("mouseover", () => {
-//   gsap.fromTo(imagediv, { scale: 1.05 }, { scale: 1, duration: 0.75 });
-// });
+const imagediv = document.querySelector(".recentimg");
+
+document.querySelector(".imagediv").addEventListener("mouseover", () => {
+  gsap.fromTo(imagediv, { scale: 1.05 }, { scale: 1, duration: 0.75 });
+});
